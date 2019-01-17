98cdf004aefb3f4560e2f0ab9724183217a4f937
Muntasir Joarder <muntasir.joarder@gmail.com>
2019-01-17 14:50:26 +1000

    Netlify deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@muntasirjoarder`,
     twitterlink: process.env.TWITTER,
     facebookpage: process.env.FACEBOOK,
     instagramlink: process.env.INSTAGRAM,
